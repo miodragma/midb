3727041638aa1c680e0f1c209dd42f9cb819706a
Miodragma <miodrag.maric86@gmail.com>
2019-11-09 03:10:51 +0100

    Feature#0019 fix navigation route

--- src/app/modules/shared/components/list-data/pages/list-data/list-data.page.ts
+++ src/app/modules/shared/components/list-data/pages/list-data/list-data.page.ts
@@ -1,9 +1,9 @@
 import { ViewChild } from '@angular/core';
 import { IonContent } from '@ionic/angular';
 import { Observable, of, Subscription } from 'rxjs';
-import { filter, map, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { MovieResponse } from '../../../../interfaces/movies/movie-response.interface';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { LoaderService } from '../../../../services/loader.service';
 import { MovieData } from '../../interfaces/movie-data.interface';
 
@@ -13,6 +13,7 @@ export class ListDataPage<T, S extends MovieData<T>> {
 
   private _value = '';
   private _type = 'popular';
+  private _page = 1;
   private _param = '';
   private _isFilter = false;
 
@@ -20,13 +21,15 @@ export class ListDataPage<T, S extends MovieData<T>> {
     of(value)
       .pipe(
         tap(val => this._value = val),
-        filter(val => !!val),
-        tap(() => this.findMovies())
       ).subscribe();
   }
 
   get value() {
     return this._value;
+  }
+
+  get type() {
+    return this._type;
   }
 
   slides$: Observable<MovieResponse<T>>;
@@ -36,53 +39,78 @@ export class ListDataPage<T, S extends MovieData<T>> {
   constructor(
     protected service: S,
     protected route: ActivatedRoute,
-    protected loaderService: LoaderService) {
+    protected loaderService: LoaderService,
+    protected router: Router) {
   }
 
   subscription: Subscription;
 
   initialization() {
-    this.subscription = this.route.queryParamMap.pipe(
-      map(param => {
-        let newParam = '';
-        param.has('with_genres') && (newParam += '&with_genres=' + param.get('with_genres'));
-        param.has('with_cast') && (newParam += '&with_cast=' + param.get('with_cast'));
-        param.has('primary_release_year') && (newParam += '&primary_release_year=' + +param.get('primary_release_year'));
-        param.has('first_air_date_year') && (newParam += '&first_air_date_year=' + +param.get('first_air_date_year'));
-        this._isFilter = !!param.get('with_genres') || !!param.get('with_cast') || !!+param.get('primary_release_year') || !!+param.get('first_air_date_year');
-        return this._isFilter ? newParam : '';
-      }),
-    ).subscribe(param => {
-      this._param = param;
+    this.subscription = this.route.queryParamMap
+      .subscribe((params: Params) => {
+        const { value, page, type, primary_release_year, with_genres, with_cast, first_air_date_year } = params.params;
+
+        this._isFilter = !!with_genres || !!with_cast || !!primary_release_year || !!first_air_date_year;
+
+        let isParam = false;
+        Object.getOwnPropertyNames(params.params).forEach(key => {
+          key && (isParam = true);
+        });
+
+        let filterQuery = '';
+        with_genres && (filterQuery += '&with_genres=' + with_genres);
+        with_cast && (filterQuery += '&with_cast=' + with_cast);
+        primary_release_year && (filterQuery += '&primary_release_year=' + +primary_release_year);
+        first_air_date_year && (filterQuery += '&first_air_date_year=' + +first_air_date_year);
+
+        type && (this._type = type);
+        !isParam && this.navigateToDefault();
+        type && this.service.findAllMoviesByType(type, +page);
+        value && this.service.findAllMoviesByValue(value, +page);
+        this._isFilter && this.service.findAllFilterMovies(filterQuery, +page);
+
       this.slides$ = this.service.findAllSlides;
       this.movies$ = this.service.findMoviesList;
-      this._param && this.service.findAllMoviesByType(undefined, undefined, this._param);
-      !this.value && !this._param && this.service.findAllMoviesByType('popular', 1, this._param);
     });
   }
 
   onSearch(value: string) {
     this.value = value;
-    !value && this.service.findAllMoviesByType(undefined, undefined, this._param);
-  }
-
-  findMovies() {
-    this.service.findAllMoviesByValue(this.value, 1, this._param);
+    if (value) {
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: { value, page: 1 }
+      });
+    }
   }
 
   selectSegment(el) {
     this._type = el.target.value;
-    this.service.findAllMoviesByType(el.target.value, undefined, this._param);
+    if (el.target.value) {
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: { type: el.target.value, page: 1 }
+      });
+    }
   }
 
-  clickScroll() {
-    this.content.scrollToTop(1000);
+  navigateToDefault() {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { type: 'popular', page: 1 }
+    });
+    this.content.scrollToTop(0);
+    this._value = undefined;
   }
 
-  more(page: number) {
-    const newPage = page + 1;
-    this._value && this.service.findMoreMoviesByValue(this._value, newPage, this._param);
-    !this._value && this.service.findMoreMoviesByType(this._type, newPage, this._param);
+  onClickedPage(page: number) {
+    this._page = page;
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { page },
+      queryParamsHandling: 'merge'
+    });
+    this.content.scrollToTop(0);
   }
 
   ionViewWillLeave() {
--- src/app/modules/shared/components/type/views/type.view.ts
+++ src/app/modules/shared/components/type/views/type.view.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { of } from 'rxjs';
+import { IonSegmentButton } from '@ionic/angular';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'type',
@@ -7,7 +10,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class TypeView {
 
+  @ViewChild('segmentButton', { static: true }) segmentButton: IonSegmentButton;
+
   @Input() segment: string;
+
+  @Input() set type(type: string) {
+    of(type)
+      .pipe(
+        tap(segmentType => {
+          segmentType && (this.segmentButton.value = segmentType);
+        })
+      )
+      .subscribe();
+  }
 
   @Output() selectSegment = new EventEmitter<CustomEvent>();
 
